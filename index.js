08132a69e4c14f35ea3fc4c48d935f3558ea7a33
Anton Winogradov <winogradovaa@gmail.com>
2015-06-24 15:37:18 +0300

    use isShorthand

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = postcss.plugin('postcss-shortcss', function(opts) {
             var nodes = rule.nodes;
 
             nodes.forEach(function(node) {
-                if(shortcss.expand(node.prop).length > 1) {
+                if(shortcss.isShorthand(node.prop)) {
                     var expandedProp = shortcss.expand(node.prop, node.value, false);
                     Object.keys(expandedProp).forEach(function(key) {
                         rule.append({ prop: key, value: expandedProp[key]});
